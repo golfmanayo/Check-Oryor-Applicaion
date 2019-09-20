5530c297722c1c76dd031d1ce380940191d3bb08
chanopkhonnasee <chanop.khonnasee@g.swu.ac.th>
2019-09-20 15:04:51 +0700

    imageToText

--- app/src/main/java/th/co/gis/cdg/checkoryorapplication/ResultActivity.kt
+++ app/src/main/java/th/co/gis/cdg/checkoryorapplication/ResultActivity.kt
@@ -1,7 +1,17 @@
 package th.co.gis.cdg.checkoryorapplication
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.cloud.FirebaseVisionCloudDetectorOptions
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.text.FirebaseVisionCloudTextRecognizerOptions
+import com.google.firebase.ml.vision.text.FirebaseVisionText
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer
 
 class ResultActivity : AppCompatActivity() {
 
@@ -10,7 +20,15 @@ class ResultActivity : AppCompatActivity() {
         setContentView(R.layout.activity_result)
     }
 
-    fun imageToText(){
-
+    fun imageToText(bitmap : Bitmap){
+        val img = FirebaseVisionImage.fromBitmap(bitmap)
+        val detector = FirebaseVision.getInstance().onDeviceTextRecognizer
+        detector.processImage(img)
+            .addOnSuccessListener { texts ->
+//                processTextRecognitionResult(texts)
+            }
+            .addOnFailureListener {
+                    e -> e.printStackTrace()
+            }
     }
 }
