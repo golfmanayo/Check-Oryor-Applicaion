0e4e8ffab5cc4caf11a52375acff3d7b487b7fee
Sira Lownoppakul <sira.l@cdg.co.th>
2019-09-20 15:07:18 +0700

    Add class find oryor

---
+++ app/src/main/java/th/co/gis/cdg/checkoryorapplication/Oryor.kt
@@ -0,0 +1,57 @@
+package th.co.gis.cdg.checkoryorapplication
+
+class Oryor {
+
+    private val foodPattern: String = "[0-9]{2}-[0-9]-[0-9]{5}-[0-9]-[0-9]{4}"
+    private val medicinePattern: String = "[0-9]{0,1}[ABCDEFGHKLMN][0-9]{1,4}\\/[0-9]{2}"
+    private val cosmeticsPattern: String = "[0-9]{2}-[0-9]-[0-9]{10}|[0-9]{2}-[0-9]-[0-9]{7}"
+    private val medicalInsPattern: String = "[ผน]\\.[0-9]{2}/[0-9]{4}"
+    private val dangerPattern: String = "วอส\\.[0-9]{2}/[0-9]{4}"
+
+    fun find(with: String):String {
+        val foodRegex = Regex(foodPattern)
+        val medicineRegex = Regex(medicinePattern)
+        val cosmeticsRegex = Regex(cosmeticsPattern)
+        val medicalInsRegex = Regex(medicalInsPattern)
+        val dangerRegex = Regex(dangerPattern)
+
+        val foodMatches = foodRegex.matches(input = with)
+        val medicineMatches = medicineRegex.matches(input = with)
+        val cosmeticsMatches = cosmeticsRegex.matches(input = with)
+        val medicalInsMatches = medicalInsRegex.matches(input = with)
+        val dangerMatches = dangerRegex.matches(input = with)
+
+        if (foodMatches) {
+            val value = foodRegex.find(with)?.value
+            if (value != null) {
+                return value
+            }
+        }
+        if (medicineMatches) {
+            val value = medicineRegex.find(with)?.value
+            if (value != null) {
+                return value
+            }
+        }
+        if (cosmeticsMatches) {
+            val value = cosmeticsRegex.find(with)?.value
+            if (value != null) {
+                return value
+            }
+        }
+        if (medicalInsMatches) {
+            val value = medicalInsRegex.find(with)?.value
+            if (value != null) {
+                return value
+            }
+        }
+        if (dangerMatches) {
+            val value = dangerRegex.find(with)?.value
+            if (value != null) {
+                return value
+            }
+        }
+
+        return ""
+    }
+}
