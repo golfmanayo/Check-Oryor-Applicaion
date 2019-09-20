9986534186ac7583be48b7d21c1901b968b7924d
Nattawood Thobood <nattawood.t@cdg.co.th>
2019-09-20 13:53:39 +0700

    first commit

---
+++ app/src/main/java/th/co/gis/cdg/checkoryorapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package th.co.gis.cdg.checkoryorapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
