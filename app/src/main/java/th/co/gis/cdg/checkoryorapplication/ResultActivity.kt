32a5c452fe5e8617cc1e9ba64bb54be8c99acc71
chanopkhonnasee <chanop.khonnasee@g.swu.ac.th>
2019-09-20 14:38:47 +0700

    ResultAct , add dependencies

---
+++ app/src/main/java/th/co/gis/cdg/checkoryorapplication/ResultActivity.kt
@@ -0,0 +1,16 @@
+package th.co.gis.cdg.checkoryorapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+
+    fun imageToText(){
+
+    }
+}
