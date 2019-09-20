a1852736fd5f7da64e47088496cddc6b146d3a5d
chanopkhonnasee <chanop.khonnasee@g.swu.ac.th>
2019-09-20 16:03:44 +0700

    test string

--- app/src/main/java/th/co/gis/cdg/checkoryorapplication/ResultActivity.kt
+++ app/src/main/java/th/co/gis/cdg/checkoryorapplication/ResultActivity.kt
@@ -12,12 +12,21 @@ import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import com.google.firebase.ml.vision.text.FirebaseVisionCloudTextRecognizerOptions
 import com.google.firebase.ml.vision.text.FirebaseVisionText
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
+
+    val oryor = Oryor()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        buttonTest.setOnClickListener {
+            val  oryortext = oryor.find("20-2-04858-2-0016")
+            val i=0
+        }
+
     }
 
     fun imageToText(bitmap : Bitmap){
@@ -25,7 +34,13 @@ class ResultActivity : AppCompatActivity() {
         val detector = FirebaseVision.getInstance().onDeviceTextRecognizer
         detector.processImage(img)
             .addOnSuccessListener { texts ->
-//                processTextRecognitionResult(texts)
+                if(texts.textBlocks.size > 0){
+                    var str = ""
+                    texts.textBlocks.forEach { blocktext ->
+                        str = str + blocktext.text
+                    }
+//                    oryor.find(str)
+                }
             }
             .addOnFailureListener {
                     e -> e.printStackTrace()
