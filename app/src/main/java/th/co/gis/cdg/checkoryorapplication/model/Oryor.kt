023cd297dab7c28277661698b8340db14f8d8ab9
chanopkhonnasee <chanop.khonnasee@g.swu.ac.th>
2019-09-20 17:43:51 +0700

    data class

--- app/src/main/java/th/co/gis/cdg/checkoryorapplication/model/Oryor.kt
+++ app/src/main/java/th/co/gis/cdg/checkoryorapplication/model/Oryor.kt
@@ -1,3 +1,19 @@
 package th.co.gis.cdg.checkoryorapplication.model
 
-data class Oryor(val Addr : String? = null)
+import com.google.gson.annotations.SerializedName
+
+data class Oryor(
+    @SerializedName("Addr") val Addr: String? = null,
+    @SerializedName("IDA") val IDA: String? = null,
+    @SerializedName("NewCode") val NewCode: String? = null,
+    @SerializedName("URLs") val URLs: String? = null,
+    @SerializedName("cncnm") val cncnm: String? = null,
+    @SerializedName("lcnno") val lcnno: String? = null,
+    @SerializedName("licen") val licen: String? = null,
+    @SerializedName("produceng") val produceng: String? = null,
+    @SerializedName("productha") val productha: String? = null,
+    @SerializedName("thanm") val thanm: String? = null,
+    @SerializedName("type") val type: String? = null,
+    @SerializedName("typeallow") val typeallow: String? = null,
+    @SerializedName("typepro") val typepro: String? = null
+    )
